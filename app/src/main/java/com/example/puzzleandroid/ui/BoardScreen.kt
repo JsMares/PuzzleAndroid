7edfc91d81529140f5a757528056225a2269bcff
Jonathan Soto <jsmares0105@gmail.com>
2026-06-20 00:37:53 -0600

    Implementar panel de control

--- app/src/main/java/com/example/puzzleandroid/ui/BoardScreen.kt
+++ app/src/main/java/com/example/puzzleandroid/ui/BoardScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -41,10 +42,21 @@ fun BoardScreen(boardViewModel: BoardViewModel = viewModel()) {
     ) {
         val itemSize = maxWidth / 9
 
-        GameBoard(
-            boardViewModel = boardViewModel,
-            itemSize = itemSize
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            ControlCustom()
+
+            GameBoard(
+                boardViewModel = boardViewModel,
+                itemSize = itemSize
+            )
+
+
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/puzzleandroid/ui/ControlCustom.kt
@@ -0,0 +1,115 @@
+package com.example.puzzleandroid.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.puzzleandroid.R
+
+@Preview(showBackground = true)
+@Composable
+fun ControlCustom() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        MovementsControl(modifier = Modifier.fillMaxHeight())
+        
+        GoalsControl(modifier = Modifier.fillMaxHeight())
+    }
+}
+
+@Composable
+private fun MovementsControl(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = Color.DarkGray,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "24",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            text = "MOVIMIENTOS",
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+private fun GoalsControl(modifier: Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = Color.DarkGray,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ItemGoals()
+    }
+}
+
+@Composable
+private fun ItemGoals() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(35.dp)
+                .clip(shape = RoundedCornerShape(6.dp))
+                .background(Color.Red.copy(alpha = 0.6f))
+                .padding(4.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.scorpion),
+                contentDescription = null
+            )
+        }
+
+        Text(
+            text = "60",
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
